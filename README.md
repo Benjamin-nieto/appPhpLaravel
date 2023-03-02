8a9f2aa1fa67515ec3acd36bad46082ce1db3f2a
agonzale110 <agonzale110@cuc.edu.co>
2023-03-01 20:32:38 -0500

    commit message

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+Realizar /login
+Realizar /consume con login y con token
+
+usar .env para poner user password y secret
+
+
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
 
 <p align="center">
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/mensaje', function () {
+    return "OK";
+});
+
+Route::post('/json', function (Request $request) {
+    $res = array(1,2,3);
+    $json = json_encode($res);
+    return $json;
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/json2',[ExampleController::class,'message']);
